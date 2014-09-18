0ac22de515cc465c083280c9d8e79b909105e20b
GeorgeLuo <gl5ed@virginia.edu>
2014-09-18 11:54:16 -0400

    Delete helloworld.m

--- helloworld.m
+++
@@ -1,9 +0,0 @@
-#include <Foundation/Foundation.h>
-
-int main(void)
-{
-    NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
-    NSLog(@"Hello World!.");
-    [pool drain];
- return;
-}
